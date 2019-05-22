ab3a1849ca5fafd7a4bc1b9028ac822861269bd2
Adin Scannell <ascannell@google.com>
2019-05-21 20:11:26 -0700

    Clean up pipe internals and add fcntl support
    Pipe internals are made more efficient by avoiding garbage collection.
    A pool is now used that can be shared by all pipes, and buffers are
    chained via an intrusive list. The documentation for pipe structures
    and methods is also simplified and clarified.
    
    The pipe tests are now parameterized, so that they are run on all
    different variants (named pipes, small buffers, default buffers).
    
    The pipe buffer sizes are exposed by fcntl, which is now supported
    by this change. A size change test has been added to the suite.
    
    These new tests uncovered a bug regarding the semantics of open
    named pipes with O_NONBLOCK, which is also fixed by this CL. This
    fix also addresses the lack of the O_LARGEFILE flag for named pipes.
    
    PiperOrigin-RevId: 249375888
    Change-Id: I48e61e9c868aedb0cadda2dff33f09a560dee773

--- abi/linux/fcntl.go
+++ abi/linux/fcntl.go
@@ -17,7 +17,6 @@ package linux
 // Comands from linux/fcntl.h.
 const (
 	F_DUPFD         = 0
-	F_DUPFD_CLOEXEC = 1030
 	F_GETFD         = 1
 	F_GETFL         = 3
 	F_GETOWN        = 9
@@ -26,6 +25,9 @@ const (
 	F_SETLK         = 6
 	F_SETLKW        = 7
 	F_SETOWN        = 8
+	F_DUPFD_CLOEXEC = 1024 + 6
+	F_SETPIPE_SZ    = 1024 + 7
+	F_GETPIPE_SZ    = 1024 + 8
 )
 
 // Flags for fcntl.
